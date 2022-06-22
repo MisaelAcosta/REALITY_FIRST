7fd886b6968e202dd079a37bbe6c5ec1f542c9df
Misael Acosta <acostamisael166@gmail.com>
2022-06-22 00:19:38 -0400

    controladore+layout

---
+++ PROYECTO/Controllers/RealityFirstController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PROYECTO.Models;
+using System.Diagnostics;
+
+namespace PROYECTO.Controllers
+{
+    public class RealityFirstController : Controller
+    {
+        IConfiguration config;
+        
+
+        public RealityFirstController(IConfiguration config)
+        {
+            this.config = config;
+            string ConnectionString = config.GetConnectionString("dbRealityFirst");
+            
+
+        }
+
+        public IActionResult Entradas()
+        {
+            return View();
+        }
+        public IActionResult Contacto()
+        {
+            return View();
+        }
+        public IActionResult Noticias()
+        {
+            return View();
+        }
+        public IActionResult Artistas()
+        {
+            return View();
+        }
+
+        public IActionResult Ingresar()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
