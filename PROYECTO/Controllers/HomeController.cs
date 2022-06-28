be96c703c398db458e1a58cf133f5deb9bebf5b9
javigarrido22 <101909212+javigarrido22@users.noreply.github.com>
2022-06-28 15:05:30 -0400

    Se genera pagina "Artistas"

--- PROYECTO/Controllers/HomeController.cs
+++ PROYECTO/Controllers/HomeController.cs
@@ -36,6 +36,11 @@ namespace Pruebas.Controllers
             return View();
         }
 
+        public IActionResult Artistas()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
