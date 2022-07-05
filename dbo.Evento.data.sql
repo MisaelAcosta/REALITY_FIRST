d2bebcb270b04147ca0f32dc8cac6e24e9c46acb
javigarrido22 <101909212+javigarrido22@users.noreply.github.com>
2022-07-04 23:19:30 -0400

    Base de datos conectada, tablas generadas y datos insertados.

---
+++ dbo.Evento.data.sql
@@ -0,0 +1,6 @@
+﻿Insert into Evento (IdEvento,evento,lugar,fechaEvento,idArtista,tipoEvento) values (1,'MOVISTAR ARENA','Santiago','05/01/2022',1,'estadio'); 
+Insert into Evento (IdEvento,evento,lugar,fechaEvento,idArtista,tipoEvento) values (2,'EstadioNacional','Santiago','15/01/2022',2,'estadio');
+Insert into Evento (IdEvento,evento,lugar,fechaEvento,idArtista,tipoEvento) values (3,'MOVISTAR ARENA','Santiago','16/04/2022',3,'estadio');
+Insert into Evento (IdEvento,evento,lugar,fechaEvento,idArtista,tipoEvento) values (4,'Teatro Caopolican','Santiago','08/12/2022',4,'teatro');
+Insert into Evento (IdEvento,evento,lugar,fechaEvento,idArtista,tipoEvento) values (5,'MOVISTAR ARENA','Santiago','05/01/2022',5,'estadio');
+Insert into Evento (IdEvento,evento,lugar,fechaEvento,idArtista,tipoEvento) values (6,'Teatro Cariola','Santiago','16/12/2022',6,'teatro');
---
+++ dbo.ticket.data.sql
@@ -0,0 +1,26 @@
+﻿Insert into ticket (Idticket,tipo,espacio,precio,idArtista)values(1,'platea  alta','teatro',20000,1);
+Insert into ticket (IdTicket,tipo,espacio,precio,idArtista)values(2,'platea  baja','teatro',10000,1);
+Insert into ticket (IdTicket,tipo,espacio,precio,idArtista)values(3,'cancha general','teatro',35000,1);
+Insert into ticket (IdTicket,tipo,espacio,precio,idArtista)values(4,'vip','teatro',50000,1);
+Insert into ticket (IdTicket,tipo,espacio,precio,idArtista)values(5,'platea  alta','estadio',40000,3);
+Insert into ticket (IdTicket,tipo,espacio,precio,idArtista)values(6,'platea  baja','estadio',40000,3);
+Insert into ticket (IdTicket,tipo,espacio,precio,idArtista)values(7,'cancha general','estadio',50000,3);
+Insert into ticket (IdTicket,tipo,espacio,precio,idArtista)values(8,'vip','estadio',60000,3);
+
+Insert into ticket (Idticket,tipo,espacio,precio,idArtista)values(9,'platea  alta','teatro',20000,2);
+Insert into ticket (IdTicket,tipo,espacio,precio,idArtista)values(10,'platea  baja','teatro',30000,2);
+Insert into ticket (IdTicket,tipo,espacio,precio,idArtista)values(11,'cancha general','teatro',50000,2);
+Insert into ticket (IdTicket,tipo,espacio,precio,idArtista)values(12,'vip','teatro',80000,2);
+Insert into ticket (IdTicket,tipo,espacio,precio,idArtista)values(13,'platea  alta','estadio',10000,4);
+Insert into ticket (IdTicket,tipo,espacio,precio,idArtista)values(14,'platea  baja','estadio',20000,4);
+Insert into ticket (IdTicket,tipo,espacio,precio,idArtista)values(15,'cancha general','estadio',10000,4);
+Insert into ticket (IdTicket,tipo,espacio,precio,idArtista)values(16,'vip','estadio',60000,4);
+
+Insert into ticket (Idticket,tipo,espacio,precio,idArtista)values(17,'platea  alta','teatro',20000,5);
+Insert into ticket (IdTicket,tipo,espacio,precio,idArtista)values(18,'platea  baja','teatro',30000,5);
+Insert into ticket (IdTicket,tipo,espacio,precio,idArtista)values(19,'cancha general','teatro',40000,5);
+Insert into ticket (IdTicket,tipo,espacio,precio,idArtista)values(20,'vip','teatro',60000,5);
+Insert into ticket (IdTicket,tipo,espacio,precio,idArtista)values(21,'platea  alta','estadio',30000,6);
+Insert into ticket (IdTicket,tipo,espacio,precio,idArtista)values(22,'platea  baja','estadio',20000,6);
+Insert into ticket (IdTicket,tipo,espacio,precio,idArtista)values(23,'cancha general','estadio',50000,6);
+Insert into ticket (IdTicket,tipo,espacio,precio,idArtista)values(24,'vip','estadio',60000,6);
